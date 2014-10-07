0d53915f6da604617b7be149c0949d210b0b2801
acgotaku <acgotaku311@gmail.com>
2014-10-07 18:36:37 +0800

    add pron

--- background.js
+++ background.js
@@ -1,41 +1,89 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'loading') {
         chrome.pageAction.show(tabId);
-        chrome.pageAction.onClicked.addListener(function(){
-            if (!chrome.runtime.onConnect.hasListeners()) {
-                chrome.runtime.onConnect.addListener(function(port) {
-                    console.assert(port.name == "ShanBayHelper");
-                    port.onMessage.addListener(function(request) {
-                        console.log(request);
-                        switch(request.method){
-                            case "getLocalStorage":
-                                sendResponse({data: localStorage});
-                                break;
-                            case "setLocalStorage":
-                                window.localStorage=request.data;
-                                sendResponse({data: localStorage});
-                                break;
-                            case 'lookup':
-                                isUserSignedOn(function() {
-                                    getClickHandler(request.data, sender.tab);
-                                });
-                                sendResponse({data:{tabid:sender.tab.id}})
-                                break;
-                            case 'addWord':
-                                addNewWordInBrgd(request.data,sendResponse);
-                                break;
-                            case 'openSettings':
-                                chrome.tabs.create({url: chrome.runtime.getURL("options.html")+'#'+request.anchor});
-                                sendResponse({data:{tabid:sender.tab.id}})
-                                break;
-                            default :
-                                sendResponse({data:[]}); // snub them.
-                        }
-                    });
+        if (!chrome.runtime.onConnect.hasListeners()) {            
+            chrome.runtime.onConnect.addListener(function(port) {
+                console.assert(port.name == "ShanBayHelper");
+                // chrome.pageAction.onClicked.addListener(function(){
+                //     port.postMessage({
+                //         method: 'toggle',
+                //         data: 'click'
+                //     });
+                //     console.log("Hello");
+                // });
+                port.onMessage.addListener(function(request) {
+                    console.log(request);
+                    switch(request.method){
+                        case "getLocalStorage":
+                            port.postMessage({data: localStorage});
+                            break;
+                        case "setLocalStorage":
+                            window.localStorage=request.data;
+                            port.postMessage({data: localStorage});
+                            break;
+                        case 'lookup':
+                            isUserSignedOn(function() {
+                                queryWord(request.data,port);
+                            });
+                            //port.postMessage({data:{tabid:sender.tab.id}})
+                            break;
+                        case 'addWord':
+                            addNewWordInBrgd(request.data,sendResponse);
+                            break;
+                        case 'openSettings':
+                            chrome.tabs.create({url: chrome.runtime.getURL("options.html")+'#'+request.anchor});
+                            port.postMessage({data:{tabid:sender.tab.id}})
+                            break;
+                        default :
+                            port.postMessage({data:[]}); // snub them.
+                    }
                 });
-            }
-            
-        });
+            });
+        }     
+
     }
 });
 
+//检查用户是否已经登录扇贝网
+function isUserSignedOn(callback) {
+    chrome.cookies.get({"url": 'http://www.shanbay.com', "name": 'username'}, function (cookie) {
+        if (cookie) {
+            localStorage.setItem('shanbay_cookies', cookie);
+            callback();
+        } else {
+            localStorage.removeItem('shanbay_cookies');
+            var url="http://www.shanbay.com/accounts/login/"
+            var opt={
+                type: "basic",
+                title: "登陆",
+                message: "登陆扇贝网后方可划词查义",
+                iconUrl: "images/logo128.png"
+            }
+            var notification = chrome.notifications.create(url,opt,function(notifyId){return notifyId});
+            chrome.notifications.onClicked.addListener( function (notifyId) {
+                chrome.notifications.clear(notifyId,function(){});
+                chrome.tabs.create({
+                    url:url
+                })
+            });
+            setTimeout(function(){
+                chrome.notifications.clear(url,function(){});
+            },5000);
+        }
+    });
+}
+//查词
+
+var API='https://api.shanbay.com/bdc/search/?word=';
+
+function queryWord(word,port){
+    var parameter = {'url': API+word, 'dataType': 'json', type: 'GET'};
+    $.ajax(parameter)
+       .done(function(json, textStatus, jqXHR){
+            console.log(json);
+            port.postMessage(json);
+       })
+       .fail(function(jqXHR, textStatus, errorThrown) {
+            $("#send_test").html(textStatus + "\u9519\u8BEF\uFF0C\u70B9\u51FB\u91CD\u65B0\u6D4B\u8BD5");
+        });
+}
--- js/shanbay.js
+++ js/shanbay.js
@@ -1,23 +1,116 @@
 (function($){
-    $(document).on('dblclick', function () {
-        var text = window.getSelection().toString().trim().match(/^[a-zA-Z\s']+$/)
-        console.info("selected "+text)
-        if (undefined != text && null!=text&&0<text.length){
-            console.log("searching "+text)
-            chrome.extension.sendMessage({
-                method: 'lookup',
-                action: 'lookup',
-                data: text[0]
-            },function(resp){
-                console.log(resp.data)
-            });
-            popover({
-                shanbay:{
-                    loading:true,
-                    msg:"查询中...."
+    var shanbay=function(){
+
+        return{
+            startListener:function(){
+                var self=this;
+                $(document).on('dblclick', function () {
+                    var text = window.getSelection().toString().trim().match(/^[a-zA-Z\s']+$/)
+                    console.info("selected "+text);
+                    if (undefined != text && null!=text&&0<text.length){
+                        console.log("searching "+text);
+                        var port = chrome.runtime.connect({name: "ShanBayHelper"});
+                        port.postMessage({
+                            method: 'lookup',
+                            data: text[0]
+                        });
+                        port.onMessage.addListener(function(response) {
+                            if(response){
+                                console.log(response);
+                                self.popover(response);
+                            }
+                        });
+                        self.popover({
+                            shanbay:{
+                                loading:true,
+                                msg:"查询中...."
+                            }
+                        })
+                    }
+                });
+            },
+            popover:function(data){
+                var html = '<div id="shanbay_popover"><div class="popover-inner"><h3 class="popover-title">';
+                if(data.status_code==0){
+                    html += '<p><span class="word">'+data.data.content+'</span>'
+                        +'<span class="pronunciation">'+(data.data.pron.length ? ' ['+data.data.pron+'] ': '')+'</span></p>'
+                        +'<a href="#" class="speak uk">UK<i class="icon icon-speak"></i></a><a href="#" class="speak us">US<i class="icon icon-speak"></i></a></h3>'
+                        +'<div class="popover-content">'
+                        +'<p>'+data.data.definition.split('\n').join("<br/>")+'</p>'
+                        +'</div>';
                 }
-            })
+
+                html += '</div></div>'
+                $('#shanbay_popover').remove()
+                $('body').append(html);
+
+                getSelectionOffset(function(left, top) {
+                    setPopoverPosition(left, top);
+                });
+
+                $('#shanbay_popover .speak.us').click(function(e) {
+                    e.preventDefault();
+                    var audio_url = 'http://media.shanbay.com/audio/us/' + data.data.content + '.mp3';
+                    playAudio(audio_url);
+                });
+
+                $('#shanbay_popover .speak.uk').click(function(e) {
+                    e.preventDefault();
+                    var audio_url = 'http://media.shanbay.com/audio/uk/' + data.data.content + '.mp3';
+                    playAudio(audio_url);
+                });
+
+                $('html').click(function() {
+                  $('#shanbay_popover').remove();
+                });
+                $('body').on('click', '#shanbay_popover', function (e) {
+                  e.stopPropagation();
+                });
+                function getSelectionOffset(callback) {
+                  var left = window.innerWidth/2;
+                  var top = window.innerHeight/2;
+                  var selection = window.getSelection();
+                  if(0<selection.rangeCount){
+                    var range = window.getSelection().getRangeAt(0);
+                    var dummy = document.createElement('span');
+                    range.insertNode(dummy);
+                    left = getLeft(dummy) - 50;
+                    top = getTop(dummy) + 25;
+                    dummy.remove();
+                    window.getSelection().addRange(range);
+                  }
+                    console.log(left + ':' + top);
+                    callback(left, top);
+                }
+                function getTop(e){
+                    var offset=e.offsetTop;
+                    if(e.offsetParent!=null) offset+=getTop(e.offsetParent);
+                    return offset;
+                }
+
+                function getLeft(e){
+                    var offset=e.offsetLeft;
+                    if(e.offsetParent!=null) offset+=getLeft(e.offsetParent);
+                    return offset;
+                }
+
+                function setPopoverPosition(left, top) {
+                    $('#shanbay_popover').css({
+                        position: 'absolute',
+                        left: left,
+                        top: top
+                    });
+                }
+                function playAudio(audio_url) {
+                    if(audio_url) {
+                        new Howl({
+                            urls: [audio_url]
+                        }).play();
+                    }
+                }
+            }
+
         }
-    });
-	
+    }();
+   shanbay.startListener();
 }(jQuery));
