5a913dcd6ddb635acebf915a6c3cffbd3a9ce146
wsy5555621 <78332317@qq.com>
2014-11-06 23:54:28 +0800

    update popup'js

---
+++ js/popup.js
@@ -0,0 +1,51 @@
+(function($){
+    var popup=function(){
+        var extension = chrome.extension.getBackgroundPage();
+        return{
+            init:function(){
+                var self=this;
+                self.startListener();
+                if(!extension.article){
+                     $("#readArticle").hide();
+                }
+            },
+            startListener:function(){
+                var self=this;
+                $("#lookup").on("click",function(){
+                    self.closePopup();
+                    var data={
+                        method: 'startListener',
+                        data: true
+                    };
+                    extension.background.sendData(data);
+                    extension.background.showNotification(true);
+                });
+                $("#readArticle").on("click",function(){
+                    self.closePopup();
+                    var data={
+                        method: 'readArticle',
+                        data: true
+                    };
+                    extension.background.sendData(data);                 
+                });
+                $("#endup").on("click",function(){
+                    self.closePopup();
+                    var data={
+                        method: 'startListener',
+                        data: false
+                    };
+                    extension.background.sendData(data);  
+                    extension.background.showNotification(false);                  
+                });
+                $("#options").on("click",function(){
+                    self.closePopup();   
+                    extension.background.openOptions();               
+                });
+            },
+            closePopup:function(){
+                window.close();
+            }
+        }
+    }();
+   popup.init();
+}(jQuery));
